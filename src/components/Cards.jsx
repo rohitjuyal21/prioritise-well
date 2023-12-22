beb3e81395be47f7c0058677d250390826a5ba3f
Rohit <rohitjuyal2003@gmail.com>
2023-12-22 15:28:43 +0530

    added local storage

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,33 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Cards = () => {
-    const [cards, setCards] = useState([]);
+    const [cards, setCards] = useState(JSON.parse(localStorage.getItem('cards')) || []);
+    const [id, setId] = useState(1);
+    // const [zIndex, setZIndex] = useState(1);
+    // Get cards from local storage
+    useEffect(() => {
+        const items = JSON.parse(localStorage.getItem('cards'));
+        if(items) {
+            console.log(cards)
+            setCards(items)
+        }
+    }, [])
+
+    // Add cards to local storage 
+    useEffect(() => {
+      localStorage.setItem('cards', JSON.stringify(cards))
+      console.log("Local storage set");
+    }, [cards])
 
     const createCard = (e) => {
         const offsetX = (e.nativeEvent.clientX / window.innerWidth) * 100
-        const offsetY = e.nativeEvent.clientY
-        setCards([...cards, { cardInput: "", isDragging: false, position: { x: offsetX, y: offsetY } }])
-        console.log(offsetX, offsetY)
+        const offsetY = e.nativeEvent.clientY;
+        
+        setCards([...cards, {id: id, cardInput: "", isDragging: false, position: { x: offsetX, y: offsetY } }])
+        setId(id+1)
     }
+
+    const inputHandler = (e, i) => {
+        const input = e.target.value
+        const updatedCards = [...cards];
+        updatedCards[i].cardInput = input;
+        setCards(updatedCards)
+    } 
 
     const mouseDown = (e, i) => {
         const updatedCards = [...cards];
+        updatedCards[i].zIndex +=  1
         updatedCards[i].isDragging = true;
         setCards(updatedCards)
-        console.log(cards)
-        console.log('x =', cards[i].position.x, 'y =', cards[i].position.y)
+
         const offsetX = (e.clientX / window.innerWidth) * 100 - updatedCards[i].position.x;
         const offsetY = e.clientY - updatedCards[i].position.y;
-        console.log("offsetx", offsetX, "offsety", offsetY)
-
+        
         const mouseMove = (ev) => {
             if (cards[i].isDragging) {
                 const updatedCards = [...cards];
                 updatedCards[i].position.x = (ev.clientX / window.innerWidth) * 100 - offsetX
                 updatedCards[i].position.y = ev.clientY - offsetY
                 setCards(updatedCards)
-                console.log('x =', cards[i].position.x, 'y =', cards[i].position.y)
-
             }
         }
 
@@ -40,15 +61,13 @@ const Cards = () => {
         }
 
         document.addEventListener('mousemove', mouseMove)
-
         document.addEventListener('mouseup', mouseUp)
 
     }
-
+    
     const deleteCard = (i) => {
         setCards(cards => cards.filter((item, index) => index !== i))
     }
-
 
     return (
         <div className='cards'>
@@ -60,7 +79,7 @@ const Cards = () => {
                     style={{ left: `${card.position.x}%`, top: `${card.position.y}px` }}
                 >
                     <div className='card__header'>
-                        <h2 className='card__number'>Card {i + 1}</h2>
+                        <h2 className='card__number'>Card {card.id}</h2>
                         <div
                             className='card__header--abs'
                             onMouseDown={e => mouseDown(e, i)}
@@ -74,7 +93,12 @@ const Cards = () => {
                         </span>
                     </div>
                     <div className='card__input--container'>
-                        <textarea name="" id="" rows="6" placeholder='Type here' className='card__input'></textarea>
+                        <textarea 
+                        rows="4" 
+                        placeholder='Type here' 
+                        className='card__input'
+                        spellCheck='false'
+                        onInput={(e) => inputHandler(e, i)}></textarea>
                     </div>
                 </div>
             ))}
